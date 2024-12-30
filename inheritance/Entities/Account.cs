d0366560e6147bcc2cc3bcef8119b872b9586665
simorai <109289730+simorai@users.noreply.github.com>
2024-12-30 15:41:50 +0000

    Creating the Account class with the account constructor and the method withdraw/deposit

--- inheritance/Entities/Account.cs
+++ inheritance/Entities/Account.cs
@@ -1,6 +1,29 @@
 ﻿namespace inheritance.Entities
 {
     internal class Account
-    {        
+    {
+        public int Number { get; set; }
+        public String Holder { get; set; }
+        public double Balance { get; set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void WithDraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double ammount)
+        {
+            Balance += ammount;
+        }
+
     }
 }
